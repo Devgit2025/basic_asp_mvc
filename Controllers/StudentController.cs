16c13a673be357d236d655c18b94349c86f81257
devgit2025 <github2025.01@gmail.com>
2025-07-09 15:11:35 +0700

    add model

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp_mvc.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();        
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+    }
+}
